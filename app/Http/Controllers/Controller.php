e01beb1850cdd4a9bb99ec58308364bd5d267d03
John Boehr <john@johnboehr.is>
2023-09-12 19:15:43 -0700

    Setup example from gh-9

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,15 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function get(\Illuminate\Http\Request $request)
+    {
+        $x = $request->validate(['values.*' => 'required|integer'])['values'];
+        \PHPStan\dumpType($x);
+        $t = collect($x);
+
+        $val = [1, '1'];
+        \PHPStan\dumpType($val); // array<int|string, int|numeric-string>
+        collect($val); // No errors
+    }
 }
